945d579d594632e4001db81ff73157f9f7d6b5d3
unknown <rakibjahan@iut-dhaka.edu>
2018-02-03 00:34:14 +0600

    02-listview

--- app/src/main/java/com/example/i_tech/onlineshop/Controller/MainActivity.kt
+++ app/src/main/java/com/example/i_tech/onlineshop/Controller/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.i_tech.onlineshop.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import com.example.i_tech.onlineshop.Model.Category
 import com.example.i_tech.onlineshop.R
+import com.example.i_tech.onlineshop.Services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var adapter : ArrayAdapter<Category>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        adapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                DataService.categories)
+        categoryListView.adapter = adapter
+
     }
 }
--- app/src/main/java/com/example/i_tech/onlineshop/Model/Category.kt
+++ app/src/main/java/com/example/i_tech/onlineshop/Model/Category.kt
@@ -4,5 +4,7 @@ package com.example.i_tech.onlineshop.Model
  * Created by i-tech on 2/2/2018.
  */
 class Category(val title:String,val image:String){
-
+    override fun toString(): String {
+        return title
+    }
 }
